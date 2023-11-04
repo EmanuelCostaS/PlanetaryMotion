80dc5be864d409cdcd713616f62922f16d1a31d1
Emanuel Silva <emanuel_silva2004@hotmail.com>
2023-11-04 14:55:53 -0300

    Some code organization.

--- scripts/Main.py
+++ scripts/Main.py
@@ -1,12 +1,15 @@
-#AnimationByMyself
+
 import matplotlib.animation as animation 
 import matplotlib.pyplot as plt
 from matplotlib.patches import Circle
 from matplotlib.patches import Ellipse
 import numpy as np
 
+#Global variables
 centrosol = (0, 0)
 solradio = (0.5)
+t_increment = 0.01
+
 plt.style.use('dark_background')
 
 #Figures
@@ -36,27 +39,27 @@ line, = ax.plot([], [], 'o')
 
 xdata, ydata = [], []
 
-
 #Animation
 def animate(i):
   
-    # t is a parameter which varies 
-	# with the frame number 
+    # t is a parameter which varies with the frame number 
 	t = 0.05 * i
 	
 	# x, y values to be plotted 
 	x = 3 + 6.7 * np.sin(t) 
 	y = 3 * np.cos(t) 
 	
-    # calculate distance to the sun
+    # calculate and print the distance to the sun
 	r = round(np.sqrt((x - centrosol[0])**2 + (y - centrosol[1])**2), 2)
-	print(f"Distance to the sun at frame: {r}")
+
+	print(f"Distance to the sun at frame: {r}, i is {i}, t is {round(t, 2)}, x is {round(x, 2)} and y is {round(y, 2)}")
 	
-	# appending values to the previously 
-	# empty x and y data holders 
+	# appending values to the previously empty x and y data holders 
 	xdata.append(x) 
 	ydata.append(y) 
 	line.set_data(x, y) 
+
+	t -= t_increment
 
 	return line, 
 
